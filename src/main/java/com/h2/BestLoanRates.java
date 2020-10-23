d98140c4c2d7dff33348d4a078da919311508a64
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-23 12:11:01 +0200

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class BestLoanRates {
 
     public static void main (String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine(); 
+        System.out.println("Hello " + name);
 
 
     }
