c00b0cde52ddcb887c263651fbd6dcdc942423f5
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-23 16:00:36 +0200

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,5 +1,6 @@
 package com.h2;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class BestLoanRates {
@@ -12,4 +13,11 @@ public class BestLoanRates {
 
 
     }
+
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
+
 }
