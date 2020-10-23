f18ef660f39523d64de330657ae7aa5808185f1a
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-23 12:01:08 +0200

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,8 +1,17 @@
 package com.h2;
 
+import java.util.Scanner;
+
 public class BestLoanRates {
 
     public static void main (String[] args){
+        Scanner scanner(System.in);
+        System.out.println("Enter your name : ");
+        scanner.nextLine(){
+            String name;
+            System.out.println("Hello " + name );
+        }
+
 
     }
 }
