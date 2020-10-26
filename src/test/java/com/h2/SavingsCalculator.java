773d97eeff8276fa7096fff96b4d869a16ffc2d1
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-26 11:10:24 +0100

    Update SavingsCalculator.java

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -10,7 +10,11 @@ public class SavingsCalculator{
     }
 
     private float sumOfCredits(){
-        return 0.0f;
+        float sum = 0.0f;
 
+        for(float credit : credits)
+            sum += credit;
+
+        return sum;
     }
 }
