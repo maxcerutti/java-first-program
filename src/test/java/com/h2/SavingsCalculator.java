bc68caf8874f84d2f655c5f4879fc3f9a76f364f
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-26 10:59:04 +0100

    Update SavingsCalculator.java

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -5,7 +5,7 @@ public class SavingsCalculator{
     private float debits[];
 
     public void SavingsCalculatorConstructor(float[] credits, float[] debits){
-        credits[] = null;
-        debits[] = null;
+        credits = null;
+        debits = null;
     }
 }
