d904d8c71614b3a145b4e12938b240b933055ef2
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-26 09:41:10 +0100

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -15,7 +15,7 @@ public class BestLoanRates {
         float getRates = getRates(loanTermInYears);
 
         if(getRates == 0.0f)
-            System.out.println( "No available rates for terms: " + loanTermInYears + " years");
+            System.out.println( "No available rates for term: " + loanTermInYears + " years");
         else
             System.out.println( "Best Available Rate: " + getRates(loanTermInYears) + "%" );
 
