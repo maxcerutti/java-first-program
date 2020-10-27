65a0949d67301f2cf0730c8a547f464813997882
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-27 09:58:16 +0100

    Create MortgageCalculator.java

---
+++ src/test/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,14 @@
+package com.h2;
+
+public class MortgageCalculator {
+    private long loanAmount = 0;
+    private int termInYears = 0;
+    private float annualRate = 0.0f;
+    private double monthlyPayment = 0d;
+
+    public constructor(){
+        long this.loanAmount = loanAmount;
+        int this.termInYears = termInYears;
+        float this.annualRate = annualRate;
+    }
+}
