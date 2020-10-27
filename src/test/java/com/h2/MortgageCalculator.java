9fc8ae186e97423797bdc7b2aae02d10f088ae4e
maxcerutti <73281818+maxcerutti@users.noreply.github.com>
2020-10-27 10:17:32 +0100

    Update MortgageCalculator.java

--- src/test/java/com/h2/MortgageCalculator.java
+++ src/test/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
     private long loanAmount = 0;
     private int termInYears = 0;
@@ -27,5 +29,10 @@ public class MortgageCalculator {
         int n = getNumberOfPayments();
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
         this.monthlyPayment = M;
+    }
+
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
     }
 }
